11a1f7567e5810448672d67194aa4a58903fecda
Ankit Raj <abhinavanand4096@gmail.com>
2025-09-03 23:05:55 +0530

    Connected Firebase
    Firebase was connected and the firebase_options.dart file is publicly kept

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:demo/Pages/regsiter.dart';
+import 'package:demo/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -10,10 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
-      
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: RegisterPage());
   }
 }
